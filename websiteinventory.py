7b519ec7913ebca3a7690369ff5e46d0dc441b8b
desiandrianti <desiandrianti363@gmail.com>
2025-06-11 09:13:23 +0700

    Create websiteinventory.py

---
+++ websiteinventory.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("Inventory Dashboard WIKA Beton")
+st.markdown("Selamat Datang di Dashboard Monitoring Persediaan WIKA Beton.")
