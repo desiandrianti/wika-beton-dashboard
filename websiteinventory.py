6046d20cccc796e6ae9cc2a583f0284a261e5fb9
desiandrianti <desiandrianti363@gmail.com>
2025-06-26 09:11:26 +0700

    Delete websiteinventory.py

--- websiteinventory.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout="wide")
-
-st.title("Inventory Dashboard WIKA Beton")
-st.markdown("Selamat Datang di Dashboard Monitoring Persediaan WIKA Beton.")
